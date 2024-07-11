a86de41b700f9007df62b2a0d8ec6e3d080ed03b
Julen <juliantxo14@gmail.com>
2024-07-11 20:07:48 +0200

    Solved pv tables test error

--- test/pvtableTest.cpp
+++ test/pvtableTest.cpp
@@ -6,7 +6,7 @@
 using namespace Xake;
 
 
-class PCTableTest : public ::testing::Test
+class PVTableTest : public ::testing::Test
 {
     protected:
     virtual void SetUp()
@@ -22,23 +22,25 @@ class PCTableTest : public ::testing::Test
     Position position;
 };
 
-TEST(PVTableTest, LoadPVLine){
+TEST_F(PVTableTest, LoadPVLine){
 
     //Inser new PV line
 
     Position position;
+    const std::string FEN_INIT_POS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    position.set_FEN(FEN_INIT_POS);
+
     PVTable::PVLine pvLine;
                                                                 
     PVTable::load_pv_line(pvLine, MAX_DEPTH, position);
                                     
     EXPECT_EQ(0, pvLine.depth);
 
-    const std::string WAC1_POSITION = "rnbqk3/PPpBpppP/8/1Q2N3/8/7K/R15p/8 w KQkq c3 34 1";
+    const std::string WAC1_POSITION = "2rr3k/pp3pp1/1nnqbN1p/3pN3/2pP4/2P3Q1/PPB4P/R4RK1 w - - 0 1";
     position.set_FEN(WAC1_POSITION);
 
     Square120 fromg3 = Square120::SQ120_G3;
     Square120 tog6 = Square120::SQ120_G6;
-
     SpecialMove spNo = SpecialMove::NO_SPECIAL;
     Piece capNo = Piece::NO_PIECE;
 
@@ -54,10 +56,9 @@ TEST(PVTableTest, LoadPVLine){
 
     Square120 fromd6 = Square120::SQ120_D6;
     Square120 toe5 = Square120::SQ120_E5;
-
     Piece capwk = Piece::W_KNIGHT;
 
-    Move md6e5 = make_move(fromg3, tog6, spNo, capwk);
+    Move md6e5 = make_move(fromd6, toe5, spNo, capwk);
     PVTable::insert_entry(position, md6e5);
 
     PVTable::load_pv_line(pvLine, MAX_DEPTH, position);
@@ -74,7 +75,6 @@ TEST(PVTableTest, LoadPVLine){
 
     //Inser new PV line
 
-    const std::string FEN_INIT_POS = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     position.set_FEN(FEN_INIT_POS);
 
     PVTable::load_pv_line(pvLine, MAX_DEPTH, position);
