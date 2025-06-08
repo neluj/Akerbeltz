56f873408e746ec3a20fb6ad0e0a97dbf620f484
Julen <juliantxo14@gmail.com>
2025-06-08 16:07:14 +0200

    #56 Update MOVE using 64 bits squares

--- test/moveTest.cpp
+++ test/moveTest.cpp
@@ -4,24 +4,24 @@
 using namespace Xake;
 
 /*Move:
-0000 0000 0000 0000 0000 0000 0000 0111 1111 -> From 0x7F
-0000 0000 0000 0000 0000 0011 1111 1000 0000 -> To 0x7F
-0000 0000 0000 0000 0000 1100 0000 0000 0000 -> Special move flag
-0000 0000 0000 0000 0111 0000 0000 0000 0000 -> Promotion Piece
-0000 0000 0000 0111 1000 0000 0000 0000 0000 -> Captured piece
-1111 1111 1111 1000 0000 0000 0000 0000 0000 -> Score
+0000 0000 0000 0000 0000 0000 0000 0011 1111 -> From 0x7F
+0000 0000 0000 0000 0000 0000 1111 1100 0000 -> To 0x7F
+0000 0000 0000 0000 0000 0011 0000 0000 0000 -> Special move flag
+0000 0000 0000 0000 0001 1100 0000 0000 0000 -> Promotion Piece
+0000 0000 0000 0001 1110 0000 0000 0000 0000 -> Captured piece
+1111 1111 1111 1110 0000 0000 0000 0000 0000 -> Score
 */
 
 TEST(MoveTest, PromotionMove){
                                                                 
-    Square120 from = Square120::SQ120_A6;                       
-    Square120 to = Square120::SQ120_A7;                         
+    Square64 from = Square64::SQ64_A6;                       
+    Square64 to = Square64::SQ64_A7;                         
     SpecialMove specialMove = SpecialMove::PROMOTION_QUEEN;  
     Piece attackerPiece = Piece::W_PAWN;
     Piece capturedPiece = Piece::B_BISHOP; 
 
     Move moveQueenPromotion = make_capture_move(from, to, specialMove, attackerPiece, capturedPiece);
-    Move expected = 0b1000011110111010010100011000111;
+    Move expected = 0b1110100111101110100110000101000;
 
     EXPECT_EQ(moveQueenPromotion, expected);
 
@@ -29,14 +29,14 @@ TEST(MoveTest, PromotionMove){
 
 TEST(MoveTest, CastlingMove){
 
-    Square120 from = Square120::SQ120_A6;                           
-    Square120 to = Square120::SQ120_A7;                             
+    Square64 from = Square64::SQ64_A6;                           
+    Square64 to = Square64::SQ64_A7;                             
     SpecialMove specialMove = SpecialMove::CASTLE;  
     Piece attackerPiece = Piece::W_PAWN;                 
     Piece capturedPiece = Piece::B_BISHOP;
     
     Move moveCasling = make_capture_move(from, to, specialMove, attackerPiece, capturedPiece);
-    Move expected = 0b1000011110110001110100011000111;
+    Move expected = 0b1110100111101100011110000101000;
 
     EXPECT_EQ(moveCasling, expected);
 
@@ -44,7 +44,7 @@ TEST(MoveTest, CastlingMove){
 
 TEST(MoveTest, MoveScoreFunction){
 
-    Move move = 0b111111111111100000000000000000000000;
+    Move move = 0b1111111111111000000000000000000000;
     MoveScore expectedScore = 8191;
     MoveScore score = move_score(move);
 
